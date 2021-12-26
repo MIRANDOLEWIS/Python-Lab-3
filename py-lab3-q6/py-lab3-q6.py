f71f0fce0f92ae29b862c4481d46e736ffa2cef4
MIRANDOLEWIS <94697301+MIRANDOLEWIS@users.noreply.github.com>
2021-12-26 12:04:06 +0530

    python lab submission

--- py-lab3-q6/py-lab3-q6.py
+++ py-lab3-q6/py-lab3-q6.py
@@ -1,11 +1,11 @@
 list = ["jerin\n","ajay\n","deepak\n"]
 
-f = open('myfile.txt', 'a')
+f = open('ans.txt', 'a')
 f.writelines(list)
 f.close()
  
 
-f = open('myfile.txt', 'r')
+f = open('ans.txt', 'r')
 lines = f.readlines()
 
 for x in lines :
